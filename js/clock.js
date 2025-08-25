83aaa4ca955cf2a332bf1725fcb927112a1d572e
Tommi Rintala <tommi.rintala@delektre.fi>
2025-08-25 19:57:55 +0300

    Fixed comment

--- js/clock.js
+++ js/clock.js
@@ -28,7 +28,8 @@ function showTime() {
 }
 
 /**
- * On page load, set interval loader (for next page)
+ * On page load, set interval loader to update time display on page.
+ * This is done every second.
  */
 document.addEventListener("DOMContentLoaded", (event) => {
     setInterval(showTime, 1000);    
